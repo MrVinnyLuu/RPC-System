a02ebe516dcbd7b4b086c870a5579ceb6f59fa6c
Vincent Luu <luuvl@student.unimelb.edu.au>
2023-05-17 16:46:02 +1000

    Test non-blocking

--- rpc.c
+++ rpc.c
@@ -1,3 +1,5 @@
+#define NONBLOCKING
+
 #include "rpc.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +13,7 @@
 #define INIT_FUNCS_SIZE 10
 #define LISTEN_QUEUE_LEN 10
 #define HEADER_LEN 5
-#define DEBUG 0
+#define DEBUG 1
 
 /********************************* SHARED API *********************************/
 
