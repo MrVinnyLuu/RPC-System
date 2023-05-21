df2c7cff837002bd517f8839e624ba1ebb28a00c
Vincent Luu <luuvl@student.unimelb.edu.au>
2023-05-21 00:00:04 +0000

    Switch hard coded cli

--- client.c
+++ client.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     int exit_code = 0;
 
     // rpc_client *state = rpc_init_client("::1", 3000);
-    rpc_client *state = rpc_init_client(argv[2], atoi(argv[4]));
+    rpc_client *state = rpc_init_client(argv[4], atoi(argv[2]));
     if (state == NULL) {
         exit(EXIT_FAILURE);
     }
